077ef5faf2d1aa89ef449097efa31f8eec1930fd
vkbt <vkbt@pm.me>
2023-03-09 19:41:39 -0500

    updating schemas

--- Schema/schmea.sql
+++ Schema/schmea.sql
@@ -51,6 +51,58 @@ CREATE TABLE "local" (
      )
 );
 
+CREATE TABLE "stores" (
+    "FIPS" int   NOT NULL,
+    "State" varchar(40)   NOT NULL,
+    "County" varchar(40)   NOT NULL,
+    "PCH_GROC_11_16" float   NOT NULL,
+    "GROCPTH11" float   NOT NULL,
+    "GROCPTH16" float   NOT NULL,
+    "PCH_GROCPTH_11_16" float   NOT NULL,
+    "PCH_SUPERC_11_16" float   NOT NULL,
+    "SUPERCPTH11" float   NOT NULL,
+    "SUPERCPTH16" float   NOT NULL,
+    "PCH_SUPERCPTH_11_16" float   NOT NULL,
+    "PCH_CONVS_11_16" float   NOT NULL,
+    "CONVSPTH11" float   NOT NULL,
+    "CONVSPTH16" float   NOT NULL,
+    "PCH_CONVSPTH_11_16" float   NOT NULL,
+    "PCH_SPECS_11_16" float   NOT NULL,
+    "SPECSPTH11" float   NOT NULL,
+    "SPECSPTH16" float   NOT NULL,
+    "PCH_SPECSPTH_11_16" float   NOT NULL,
+    CONSTRAINT "pk_stores" PRIMARY KEY (
+        "FIPS"
+     )
+);
+
+CREATE TABLE "restaurants" (
+    "FIPS" int   NOT NULL,
+    "State" varchar(40)   NOT NULL,
+    "County" varchar(40)   NOT NULL,
+    "PCH_FFR_11_16" float   NOT NULL,
+    "FFRPTH11" float   NOT NULL,
+    "FFRPTH16" float   NOT NULL,
+    "PCH_FFRPTH_11_16" float   NOT NULL,
+    "PCH_FSR_11_16" float   NOT NULL,
+    "FSRPTH11" float   NOT NULL,
+    "FSRPTH16" float   NOT NULL,
+    "PCH_FSRPTH_11_16" float   NOT NULL,
+    "PC_FFRSALES07" float   NOT NULL,
+    "PC_FFRSALES12" float   NOT NULL,
+    "PC_FSRSALES07" float   NOT NULL,
+    "PC_FSRSALES12" float   NOT NULL,
+    CONSTRAINT "pk_restaurants" PRIMARY KEY (
+        "FIPS"
+     )
+);
+
 ALTER TABLE "local" ADD CONSTRAINT "fk_local_FIPS" FOREIGN KEY("FIPS")
+REFERENCES "insecurity" ("FIPS");
+
+ALTER TABLE "stores" ADD CONSTRAINT "fk_stores_FIPS" FOREIGN KEY("FIPS")
+REFERENCES "insecurity" ("FIPS");
+
+ALTER TABLE "restaurants" ADD CONSTRAINT "fk_restaurants_FIPS" FOREIGN KEY("FIPS")
 REFERENCES "insecurity" ("FIPS");
 
